12e68b4b142167347f84399e9a14dfb07d4cdd69
Mateusz <Mateusz@DESKTOP-JJED8DN>
2022-10-29 13:49:11 +0200

    Add project files.

---
+++ MvcLibrary/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        
+        [Column(TypeName  = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ MvcLibrary/Models/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcLibrary.Data;
+using System;
+using System.Linq;
+
+namespace MvcLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialiaze(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcLibraryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcLibraryContext>>()))
+            {
+                // Look for any movies.
+                if (context.Book.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                new Book
+                {
+                    Title = "The Witcher Sword of Destiny",
+                    ReleaseDate = DateTime.Parse("1992-01-01"),
+                    Genre = "Fantasy",
+                    Price = 29
+                },
+
+                new Book
+                {
+                    Title = "The Witcher The Last Wish",
+                    ReleaseDate = DateTime.Parse("1993-01-01"),
+                    Genre = "Fantasy",
+                    Price = 39
+                },
+
+                new Book
+                {
+                    Title = "The Witcher Blood of Elves",
+                    ReleaseDate = DateTime.Parse("1994-01-01"),
+                    Genre = "Fantasy",
+                    Price = 34,
+                }
+            );
+                context.SaveChanges();
+            }
+        }
+    }
+}
