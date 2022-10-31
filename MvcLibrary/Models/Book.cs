ed9f68cdf9ba052ca4fa3e79200bba230ad9ddd3
mwasilew <01158977@pw.edu.pl>
2022-10-31 19:14:36 +0100

    Added validation to model Book

--- MvcLibrary/Models/Book.cs
+++ MvcLibrary/Models/Book.cs
@@ -7,15 +7,25 @@ namespace MvcLibrary.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
         public string? Title { get; set; }
 
         [Display(Name = "Publish Date")]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
         public string? Genre { get; set; }
 
         [Display(Name = "Pages Number")]
         [Column(TypeName  = "decimal(18, 2)")]
+        [Range(1,10000)]
+        [Required]
         public decimal PagesNumber { get; set; }
     }
 }
