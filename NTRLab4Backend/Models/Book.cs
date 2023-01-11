54aa43d7b261f4fd4393726657ae13dee3e6bc0d
mwasilew <01158977@pw.edu.pl>
2023-01-11 07:42:23 +0100

    Imported Book and User models from previous labs

---
+++ NTRLab4Backend/Models/Book.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTRLab4Backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string? Title { get; set; }
+
+        [RegularExpression(@"^[-\'A-Za-z\s]$")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string? Author { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string? Genre { get; set; }
+
+        [Display(Name = "Pages Number")]
+        [Column(TypeName  = "decimal(18, 0)")]
+        [Range(1,10000)]
+        [Required]
+        public int PagesNumber { get; set; }
+
+        [ForeignKey("User")]
+        public int? UserId { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime? ReservedUntil { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime? LentUntil { get; set; }
+
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+    }
+}
---
+++ NTRLab4Backend/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTRLab4Backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, MinimumLength = 1)]
+        [Required]
+        public string Username { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(20, MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        [Column(TypeName = "decimal(1,0)")]
+        [Range(0, 1)]
+        [Required]
+        public int IsLibrarian { get; set; } = 0;
+
+        [DataType(DataType.Date)]
+        public DateTime? TimeStamp { get; set; } = DateTime.Now;
+    }
+}
