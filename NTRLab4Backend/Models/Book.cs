5c335df3d3677cc67dadd9eb5c609e53a8bd8cb2
mwasilew <01158977@pw.edu.pl>
2023-01-11 07:43:56 +0100

    Removed some annotations from Book and User models

--- NTRLab4Backend/Models/Book.cs
+++ NTRLab4Backend/Models/Book.cs
@@ -8,29 +8,16 @@ namespace NTRLab4Backend.Models
     {
         public int Id { get; set; }
 
-        [StringLength(100, MinimumLength = 1)]
-        [Required]
         public string? Title { get; set; }
 
-        [RegularExpression(@"^[-\'A-Za-z\s]$")]
-        [StringLength(100, MinimumLength = 1)]
-        [Required]
         public string? Author { get; set; }
 
-        [Display(Name = "Publish Date")]
         [DataType(DataType.Date)]
-        [Required]
         public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [StringLength(100, MinimumLength = 1)]
-        [Required]
         public string? Genre { get; set; }
 
-        [Display(Name = "Pages Number")]
         [Column(TypeName  = "decimal(18, 0)")]
-        [Range(1,10000)]
-        [Required]
         public int PagesNumber { get; set; }
 
         [ForeignKey("User")]
