54b21cc871767c8465804801b59c3bb1d3e3f7c2
mwasilew <01158977@pw.edu.pl>
2023-01-12 02:01:59 +0100

    Backend: Updated WeatherForecast route

---
+++ NTRLab4Backend/Migrations/20230112004548_UpdatedWeatherForecastRoute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTRLab4Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedWeatherForecastRoute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
